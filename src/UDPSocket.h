63bad3a2f81d8e4723be47305c1b58f8a64380b8
Daniel Wagenaar <daw@caltech.edu>
2019-07-08 10:05:39 -0700

    UDPSocket, older

--- src/UDPSocket.h
+++ src/UDPSocket.h
@@ -1,1 +1,44 @@
-/home/wagenaar/werk/sbemviewer/src/main/UDPSocket.h
+// UDPSocket.h
+
+#ifndef UDPSOCKET_H
+
+#define UDPSOCKET_H
+
+#include <QObject>
+
+class QUdpSocket;
+
+namespace UDPSocket {
+  struct Message {
+    double x, y, z;
+    int id;
+  };
+
+  QString sbemPath();
+  QString gvoxPath();
+
+  class Server: public QObject {
+    Q_OBJECT;
+  public:
+    Server(QString path, QObject *parent=0);
+    ~Server();
+  signals:
+    void messageReceived(Message msg);
+  private:
+    QString path;
+    QUdpSocket *sock;
+  };
+
+  class Client: public QObject {
+    Q_OBJECT;
+  public:
+    Client(QString path, QObject *parent=0);
+    ~Client();
+  public slots:
+    void sendMessage(Message msg);
+  private:
+    QString path;
+  };    
+};
+
+#endif
